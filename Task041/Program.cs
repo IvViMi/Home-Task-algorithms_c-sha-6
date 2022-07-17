5315f76778d1da06f25611972bc558888fba4f8a
Viataly <q08642@yandex.ru>
2022-07-17 21:38:34 +0300

    add T041

---
+++ Task041/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 41: 
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+Console.WriteLine("Определение количества чисел, соответсвующих условию: '> 0', которые ввёл пользователь");
+Console.Write("Введите целые числа через один пробел и нажмите ввод: ");
+int[] array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+int Nums(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+Console.WriteLine($"Количество чисел '> 0': {Nums(array)} шт.");
